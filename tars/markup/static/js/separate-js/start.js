c6ac62f5251a1e668f5e7e02d3ee0cb4e53aec93
zupra <dima5447298@gmail.com>
2016-03-04 16:41:31 +0300

    svg

--- tars/markup/static/js/separate-js/start.js
+++ tars/markup/static/js/separate-js/start.js
@@ -47,9 +47,7 @@ jQuery(function ($) {
 */
 
 	$(document)
-//			.on('click','.lang-list .item',function(e){
-//				var el = $(this);
-//			})
+
 			.on('click','.wrap-form .btn-close',function(e){
 				e.preventDefault();
 				var el = $(this);
@@ -163,7 +161,6 @@ jQuery(function ($) {
 
 			.on('click', '.js_expander-trigger', function(e){
 				//e.preventDefault();
-				//alert(1);
 				$(this).next('.js_expander-content').slideToggle();
 			});
 
@@ -172,6 +169,54 @@ jQuery(function ($) {
 			// $('.js_expander-trigger').click(function(){
 		  //   $(this).toggleClass("expander-hidden");
 		  // });
+
+
+			/*
+
+			$('.accordion-tabs-minimal').each(function(index) {
+				$(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+			});
+			$('.accordion-tabs-minimal').on('click', 'li > a.tab-link', function(event) {
+				if (!$(this).hasClass('is-active')) {
+					event.preventDefault();
+					var accordionTabs = $(this).closest('.accordion-tabs-minimal');
+					accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+					$(this).next().toggleClass('is-open').toggle();
+					accordionTabs.find('.is-active').removeClass('is-active');
+					$(this).addClass('is-active');
+				} else {
+					event.preventDefault();
+				}
+			});
+
+
+
+
+
+			$('section').each(function() {
+		    // var size = $('.js-tab').size(); //.eq(0).show();
+		    // console.log(size);
+		    $(this).find('.js-tab').eq(0).show();
+		  });
+
+		  $('.js-tabNav a').click(function(e) {
+
+
+		    var index = $(this).index(),
+		        tab = $(this).parent('.js-tabNav').nextAll('.js-tab'),
+		        current_tab = tab.get(index);
+
+		    console.log(current_tab);
+		    $(this).addClass('tab_current').siblings().removeClass('tab_current');
+		    $(current_tab).show().siblings('.js-tab').hide();
+
+		    return false;
+		  });
+
+
+*/
+
 
 	//Swiper
 	var sliderOption = {
